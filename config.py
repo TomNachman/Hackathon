f451bd754b2ba8fd67d15104b40e68ef86e373f2
ShaharWeisberger <81864845+ShaharWeisberger@users.noreply.github.com>
2021-12-28 16:32:25 +0200

    Update config.py
    Final after the changes

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 """
 for this assignment this file is .py but it should by a config file ( .ini for example) and should be under .gitignore
 """
+import random
+from typing import Tuple, Union
+
 # GLOBALS - CONSTANTS
 UDP_PORT = 13117
 TCP_PORT = 2016
@@ -18,3 +21,20 @@ FAIL = '\033[91m'
 END = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
+RED = '\033[91m'
+BG_GREEN = '\033[42m'
+
+ops = {"+": (lambda x, y: x + y), "-": (lambda x, y: x - y), "*": (lambda x, y: x * y), "/": (lambda x, y: x / y)}
+r = range(0, 9)
+
+
+def generate_simple_math() -> Tuple[str, int]:
+    while True:
+        num1 = random.randint(1, 100)
+        num2 = random.randint(1, 100)
+        operation = random.choice(list(ops.keys()))
+        msg = f"{OK_BLUE}How much is " + str(num1) + operation + str(num2) + "?\n"
+        answer = ops[operation](num1, num2)
+        if answer in r:
+            return msg, answer
+
